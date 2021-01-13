56f9ce8633bc59e957a4fa0c50c9b456301a5ecc
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-01-14 00:50:05 +0530

    Create SecondMinMax.py

---
+++ SecondMinMax.py
@@ -0,0 +1,12 @@
+#Given a list of numbers (integers), find second maximum and second minimum in this list.
+
+#Input Format:
+#The first line contains numbers separated by a space.
+
+#Output Format:
+#Print second maximum and second minimum separated by a space
+
+
+a = [int(x) for x in input().split()]
+a.sort() #this command sorts the list in ascending order
+print(a[-2],a[1])
