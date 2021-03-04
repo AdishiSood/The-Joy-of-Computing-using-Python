2827a5a554577e30a0360b3310f937a0ab61b899
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-04 22:05:58 +0530

    Update Digits.py

--- Digits.py
+++ Digits.py
@@ -1,3 +1,24 @@
+"""
+You are given a number A which contains only digits 0's and 1's. Your task is to make all digits same by just flipping one digit (i.e. 0 to 1 or 1 to 0 ) only. If it is possible to make all the  digits same by just flipping one digit then print 'YES' else print 'NO'.
+
+Input Format:
+The first line contains a number made up of 0's and 1's.
+
+Output Format:
+Print 'YES' or 'NO' accordingly without quotes.
+
+Example:
+
+Input:
+
+101
+
+Output:
+YES
+
+Explanation: 
+If you flip the middle digit from 0 to 1 then all the digits will become same. Hence output is YES.
+"""
 list1=input()
 #print(list1)
 x=list1.count('1')
