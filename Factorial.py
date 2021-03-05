807886110112ab0a21ecb334428eb3e1aec8f378
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-05 12:15:50 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,24 +1,20 @@
-/*
+"""
 
 Given an integer number n, you have to print the factorial of this number. 
 
 Input Format:
-
 A number n.
 
 Output Format:
-
 Print the factorial of n.
 
 Example:
-
 Input:
 4
 
 Output:
 24
-
-*/
+"""
 
 k = int(input())
 fac = 1
