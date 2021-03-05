979302402c6552d4cb924013e60f4a331927bbf8
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-05 12:18:53 +0530

    Update Multiple_Of_5.py

--- Multiple_Of_5.py
+++ Multiple_Of_5.py
@@ -1,3 +1,23 @@
+"""
+Given a list A of numbers (integers), you have to print those numbers which are not multiples of 5.
+
+Input Format:
+The first line contains the numbers of list A separated by a space.
+
+Output Format:
+Print the numbers in a single line separated by a space which are not multiples of 5.
+
+Example:
+Input:
+1 2 3 4 5 6 5
+
+Output:
+1 2 3 4 6
+
+Explanation:
+Here the elements of A are 1,2,3,4,5,6,5 and since 5 is the multiple of 5, after removing them the list becomes 1,2,3,4,6.
+"""
+
 a = [int(x) for x in input().split()]
 b = []
 for i in a:
