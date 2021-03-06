ffc2204be015bece4837a342697d320e2caf19fa
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-06 09:10:25 +0530

    Update SumOfReverse.py

--- SumOfReverse.py
+++ SumOfReverse.py
@@ -1,14 +1,14 @@
+"""
+Given an array A of N numbers (integers), you have to write a program which prints the sum of the elements of array A with the corresponding elements of the reverse of array A.
+If array A has elements [1,2,3], then reverse of the array A will be [3,2,1] and the resultant array should be [4,4,4].
 
-#Given an array A of N numbers (integers), you have to write a program which prints the sum of the elements of array A with the corresponding elements of the reverse of array A.
-#If array A has elements [1,2,3], then reverse of the array A will be [3,2,1] and the resultant array should be [4,4,4].
+Input Format:
+The first line of the input contains a number N representing the number of elements in array A.
+The second line of the input contains N numbers separated by a space. (after the last elements, there is no space)
 
-#Input Format:
-#The first line of the input contains a number N representing the number of elements in array A.
-#The second line of the input contains N numbers separated by a space. (after the last elements, there is no space)
-
-#Output Format:
-#Print the resultant array elements separated by a space. (no space after the last element)
-
+Output Format:
+Print the resultant array elements separated by a space. (no space after the last element)
+""""
 
 N = int(input())
 A = [int(i) for i in input().split(" ")]
