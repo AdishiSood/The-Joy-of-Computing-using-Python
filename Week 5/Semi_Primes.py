32d55497bd68f477ed912d3b806c698f8f48988f
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-09 12:18:27 +0530

    Create Semi_Primes.py

---
+++ Week 5/Semi_Primes.py
@@ -0,0 +1,23 @@
+"""
+A semiprime number is an integer which can be expressed as a product of two distinct primes. For example 15 = 3*5 is a semiprime number but 9 = 3*3 is not .
+Given an integer number N, find whether it can be expressed as a sum of two semi-primes or not (not necessarily distinct).
+
+Input Format:
+The first line contains an integer N.
+
+Output Format:
+Print 'Yes' if it is possible to represent N as a sum of two semiprimes 'No' otherwise.
+
+Example:
+
+Input:
+30
+
+Output:
+Yes
+
+Explanation:
+N = 30 can be expressed as 15+15 where 15 is a semi-prime number (5*3 = 15)
+
+NOTE: N is less than equal to 200
+"""
