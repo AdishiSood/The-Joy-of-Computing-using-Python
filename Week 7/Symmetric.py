c54460bc62104147402c82a3d81b2e4f1afae32b
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-12 19:09:09 +0530

    Update Symmetric.py

--- Week 7/Symmetric.py
+++ Week 7/Symmetric.py
@@ -1,1 +1,20 @@
+"""
+Given a square matrix of N rows and columns, find out whether it is symmetric or not.
 
+Input Format:
+The first line of the input contains an integer number n which represents the number of rows and the number of columns.
+From the second line, take n lines input with each line containing n integer elements with each element separated by a space.
+
+Output Format:
+Print 'YES' if it is symmetric otherwise 'NO'
+
+Example:
+
+Input:
+2
+1 2
+2 1
+
+Output:
+YES
+"""
