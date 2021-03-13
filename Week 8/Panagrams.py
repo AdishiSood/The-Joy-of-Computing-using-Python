6d9e35a694e7bc46a517331f51f40fd0f7aee292
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-13 20:53:12 +0530

    Create Panagrams.py

---
+++ Week 8/Panagrams.py
@@ -0,0 +1,15 @@
+"""
+A panagram is a sentence containing every 26 letters in the English alphabet. Given a string S, check if it is panagram or not.
+Input Format:
+The first line contains the sentence S.
+
+Output Format:
+Print 'YES' or 'NO' accordingly
+
+Example:
+Input:
+The quick brown fox jumps over the lazy dog
+
+Output:
+YES
+"""
