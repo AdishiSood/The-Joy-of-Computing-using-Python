81f45bdb15d3fdf32741df6990bbec7d5cdc4ab2
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-14 17:04:58 +0530

    Update Vowels.py

--- Week 8/Vowels.py
+++ Week 8/Vowels.py
@@ -1,1 +1,19 @@
+Given a string S of lowercase letters, remove consecutive vowels from S.
+After removing, the order of the list should be maintained.
 
+Input Format:
+Sentence S in a single line
+
+Output Format:
+Print S after removing consecutive vowels
+
+Example:
+Input:
+your article is in queue
+
+Output:
+yor article is in qu
+
+Explanation:
+In the first word, 'o' and 'u' are appearing together, hence the second letter 'u' is removed.
+In the fifth word, 'u', 'e', 'u' and 'e' are appearing together, hence 'e', 'u', 'e' are removed.
