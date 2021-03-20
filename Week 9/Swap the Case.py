8f62b7c60afa93ad61abb72bd51b99c0ebd11806
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-20 13:11:21 +0530

    Update Swap the Case.py

--- Week 9/Swap the Case.py
+++ Week 9/Swap the Case.py
@@ -1,1 +1,15 @@
+"""
+Given a string, you need to convert all lowercase letters to uppercase letters and vice versa.
 
+Input Format:
+A single line containing a string S.
+
+Output Format:
+Print the modified string S.
+
+Sample Input:
+Hello World!!
+
+Sample Output:
+hELLO wORLD!!
+"""
