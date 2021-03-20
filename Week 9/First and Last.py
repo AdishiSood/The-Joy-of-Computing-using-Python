e3203ee598ea87412f6c4bfd04ff63bbd69d561c
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-21 00:07:36 +0530

    Update First and Last.py

--- Week 9/First and Last.py
+++ Week 9/First and Last.py
@@ -1,1 +1,15 @@
+"""
+Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. If the string length is less than 2, return instead of the empty string.
 
+Input Format:
+The first line of the input contains the String S.
+
+Output Format:
+The first line of the output contains the modified string.
+
+Sample Input:
+Programming
+
+Sample Output:
+Prng
+"""
