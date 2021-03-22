a06e28d56addf31d525b826dbfbbf35700348ba7
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-22 12:58:23 +0530

    Update Rotate the matrix.py

--- Week 9/Rotate the matrix.py
+++ Week 9/Rotate the matrix.py
@@ -1,2 +1,23 @@
+
 """
+Given a square matrix with n rows and n columns, you have to write a program to rotate this matrix such that each element is shifted by one place in a clockwise manner.
+For example, given the following matrix
+
+1 2 3
+4 5 6
+7 8 9
+
+The output should be
+
+4 1 2
+7 5 3
+8 9 6
+
+Input Format:
+The first line of the input contains a number n representing the number of rows and columns.
+After this, there are n rows with each row containing n elements separated by a space
+
+Output Format:
+Print the elements of the modified matrix with each row in a new line and all the elements in each row is separated by a space.
+
 """
