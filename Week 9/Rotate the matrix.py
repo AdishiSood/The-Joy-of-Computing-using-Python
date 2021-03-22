425929018900875af15684d357b8d905c8eaa4b9
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-22 12:58:54 +0530

    Update Rotate the matrix.py

--- Week 9/Rotate the matrix.py
+++ Week 9/Rotate the matrix.py
@@ -18,5 +18,35 @@ After this, there are n rows with each row containing n elements separated by a 
 
 Output Format:
 Print the elements of the modified matrix with each row in a new line and all the elements in each row is separated by a space.
+Example 1:
+
+Input:
+3
+1 2 3
+4 5 6
+7 8 9
+
+Output:
+4 1 2
+7 5 3
+8 9 6
+
+Example 2:
+Input:
+4
+1 2 3 4
+5 6 7 8
+9 10 11 12
+13 14 15 16
+
+Output:
+5 1 2 3
+9 10 6 4
+13 11 7 8
+14 15 16 12
+
+Explanation: 
+In the first example, there is an odd number of rows and columns hence excluding the middle element i.e. 5 all the elements were shifted by one position in a clockwise manner.
+In the second example, there are even number of rows and columns hence all the elements were shifted by one position in a clockwise manner
 
 """
