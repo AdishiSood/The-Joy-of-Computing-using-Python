187b0cf46a2927e76b70288931ef247cb832dc84
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-26 16:20:57 +0530

    Update Missing Number.py

--- Week 10/Missing Number.py
+++ Week 10/Missing Number.py
@@ -26,3 +26,16 @@ print(n,end="")
 
 
 or
+
+# getMissingNo takes list as argument 
+def getMissingNo(A): 
+    n = len(A) 
+    total = (n+1)*(n+2)/2
+    sum_of_A = sum(A) 
+    return total - sum_of_A 
+  
+# Driver program to test above function 
+li=[]
+li= list(map(int, input ().split ())) 
+miss = getMissingNo(li) 
+print(int(miss)) 
