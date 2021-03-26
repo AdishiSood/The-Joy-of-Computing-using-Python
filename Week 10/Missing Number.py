9317c1a9d4ae13c274fe6e59ee86e57d1708a73a
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-26 16:20:06 +0530

    Update Missing Number.py

--- Week 10/Missing Number.py
+++ Week 10/Missing Number.py
@@ -1,2 +1,20 @@
 """
+Given a list of n-1 numbers ranging from 1 to n, your task is to find the missing number. There are no duplicates. 
+
+Input Format:
+The first line contains n-1 numbers with each number separated by a space.
+
+Output Format:
+Print the missing number
+
+Example:
+
+Input:
+1 2 4 6 3 7 8
+
+Output:
+5
+
+Explanation:
+In the above list of numbers 5 is missing and hence 5 is the input
 """
