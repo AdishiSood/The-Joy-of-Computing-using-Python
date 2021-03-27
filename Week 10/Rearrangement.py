b1dad03609a5ea233bf91ff141fef642854a5429
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-27 16:05:18 +0530

    Update Rearrangement.py

--- Week 10/Rearrangement.py
+++ Week 10/Rearrangement.py
@@ -1,1 +1,20 @@
+/*
+Given a list A of elements of length N, ranging from 1 to N. All elements may not be present in the array. If the element is not present then there will be -1 present in the array. Rearrange the array such that A[i] = i and if i is not present display -1 at that place.
 
+Input Format:
+The first line contains n numbers with each number separated by a space.
+
+Output Format:
+Print the elements of the list after the modification.
+
+Example:
+
+Input:
+-1 -1 6 1 9 3 2 -1 4 -1
+
+Output:
+-1 1 2 3 4 -1 6 -1 -1 9
+
+Explanation:
+The modified list contains elements such that A[i] = i.
+*/
