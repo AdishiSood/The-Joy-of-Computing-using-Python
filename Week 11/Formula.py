d49340a62aff7926f51325c154dfa6de730e9c13
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-04 09:56:52 +0530

    Update Formula.py

--- Week 11/Formula.py
+++ Week 11/Formula.py
@@ -32,4 +32,12 @@ for i in range(len(l)):
     
     
     or
-    
+    import math
+c=50
+h=30
+value = []
+items=[x for x in input().split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+
+print (','.join(value))
