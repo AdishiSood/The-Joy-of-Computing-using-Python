cbd45378f6620315b501bdad519cf14c88392123
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-10 20:24:25 +0530

    Update word-sorting.py

--- Week 11/word-sorting.py
+++ Week 11/word-sorting.py
@@ -1,4 +1,18 @@
+Write a program that accepts a comma-separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
 
+Input Format:
+The first line of input contains words separated by the comma.
+
+Output Format:
+Print the sorted words separated by the comma.
+
+Example:
+
+Input:
+without,hello,bag,world
+
+Output:
+bag,hello,without,world
 
 
 l=input().split(',')
