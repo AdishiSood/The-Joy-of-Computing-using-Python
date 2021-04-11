46867d6dfce3f6ad68f0df3314bddd9b3cb1af75
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-11 20:49:19 +0530

    Update Numbers.py

--- Week 11/Numbers.py
+++ Week 11/Numbers.py
@@ -25,3 +25,14 @@ for i in range(b,e+1,2):
 print(",".join(f),end="")
 
 or
+
+m,n = input().split(',')
+m = int(m)
+n = int(n)
+
+values = []
+for i in range(m, n+1):
+    s = str(i)
+    if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0):
+        values.append(s)
+print(",".join(values))
