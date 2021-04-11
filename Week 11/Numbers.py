bc60fbe8831e1076af660bda9b60305a80d8acc3
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-11 20:47:35 +0530

    Update Numbers.py

--- Week 11/Numbers.py
+++ Week 11/Numbers.py
@@ -1,2 +1,13 @@
 """
+Write a program, which will find all such numbers between m and n (both included) such that each digit of the number is an even number.
+
+Input Format:
+The first line contains value m and n separated by a comma.
+
+Output Format:
+The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+Constraints:
+1000<=m<=9000
+1000<=n<=9000
 """
