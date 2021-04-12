73343270d16fd682183fe9f92e237e44ad1f73d5
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-12 18:48:00 +0530

    Update Max and Min.py

--- Week 3/Max and Min.py
+++ Week 3/Max and Min.py
@@ -7,6 +7,12 @@ The first line contains numbers separated by a space.
 Output Format:
 Print second maximum and second minimum separated by a space
 """
+n = list(map(int, input().split()))
+n.sort()
+l=len(n)
+print(n[l-2],n[1],end="")
+
+or
 
 a = [int(x) for x in input().split()]
 a.sort() #this command sorts the list in ascending order
