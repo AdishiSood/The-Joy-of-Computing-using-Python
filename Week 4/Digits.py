32bf18c9ebe90979114af39a0adf50908c0e0e9d
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-12 18:50:19 +0530

    Update Digits.py

--- Week 4/Digits.py
+++ Week 4/Digits.py
@@ -18,6 +18,27 @@ YES
 Explanation: 
 If you flip the middle digit from 0 to 1 then all the digits will become same. Hence output is YES.
 """
+A = input()
+ls = []
+li = str(A)
+for j in li:
+    ls.append(int(j))
+count_z = 0
+count_o = 0
+for k in ls:
+    if(k==1):
+        count_o += 1
+    if(k==0):
+        count_z += 1
+
+if((count_o == 1) or (count_z == 1)):
+    print("YES")
+
+else:
+    if((count_o == 0) or (count_z == 0)):
+        print("NO")
+    else:
+        print("NO")
 or
 list1=input()
 #print(list1)
