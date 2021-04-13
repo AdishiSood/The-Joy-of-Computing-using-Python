f502746bd4b36ac85974938293dea31952ff2053
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-13 20:47:55 +0530

    Update Cab and walk.py

--- Week 5/Cab and walk.py
+++ Week 5/Cab and walk.py
@@ -42,5 +42,21 @@ else:
   
   or
 
-  
+ 
+n,v1,v2 = input().split()
+
+n = int(n)
+v1 = int(v1)
+v2 = int(v2)
+
+st_d = pow(2,1/2)*n
+st_t = st_d/v1
+
+el_d = 2*n
+el_t = el_d/v2
+
+if(st_t<el_t):
+    print("Walk")
+else:
+    print("Cab")
   
