aca4c3180ed92c1397e7c9647a5c48850e5f1e35
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-28 11:40:18 +0530

    Create Email ID.py

---
+++ Week 12/Email ID.py
@@ -0,0 +1,42 @@
+"""
+Assuming that we have some email addresses in the "username@companyname.com" format, 
+please write program to print the company name of a given email address. 
+Both user names and company names are composed of letters only.
+
+Input Format:
+The first line of the input contains an email address.
+
+Output Format:
+Print the company name in single line.
+
+Example;
+
+Input:
+john@google.com
+
+Output:
+google
+"""
+S=input()
+
+temp=""
+flag=False
+
+for i in S:
+  
+  if(i=='@'):
+    flag=True
+    continue
+  if(i=='.' and flag==True):
+    break
+  if(flag):
+    temp=temp+str(i)
+print(temp,end="")
+
+or
+
+import re
+emailAddress = input()
+pat2 = "(\w+)@(\w+)\.(com)"
+r2 = re.match(pat2,emailAddress)
+print(r2.group(2))
